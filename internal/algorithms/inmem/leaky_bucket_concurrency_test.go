e7e8afbc7eff251f9caaf4080866d42159398876
Ali Rıza Aynacı <115364922+AliRizaAynaci@users.noreply.github.com>
2025-05-16 03:45:48 +0300

    Increase tolerance in leaky bucket concurrency test from 2 to 4

--- internal/algorithms/inmem/leaky_bucket_concurrency_test.go
+++ internal/algorithms/inmem/leaky_bucket_concurrency_test.go
@@ -39,7 +39,7 @@ func TestLeakyBucketLimiter_Allow_Concurrency(t *testing.T) {
 	wg.Wait()
 
 	maxAllowed := 80
-	tolerance := 2
+	tolerance := 4
 	if successCount < maxAllowed || successCount > maxAllowed+tolerance {
 		t.Errorf("expected allowed between %d and %d, got %d", maxAllowed, maxAllowed+tolerance, successCount)
 	}
